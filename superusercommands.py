6ffae2b21081e694bba9f10c831f28d038809cfa
Alex Carr <alexcarrac123@gmail.com>
2026-04-17 14:34:21 -0500

    added a new super user command

--- superusercommands.py
+++ superusercommands.py
@@ -25,4 +25,23 @@ async def superuser_commands(message):
                     else:
                         await message.channel.send('You don\'t have permission to do that!')        
 
+        case s if s.startswith('!givemoney'):
+                    if message.author.id == 585178815253446685:
+                        if len(message.mentions) == 0:
+                            await message.channel.send('You need to @ someone!')
+                            return
+                        if len(message.content.split()) < 3:
+                            await message.channel.send('You need to specify an amount! Usage: !givemoney @user amount')
+                            return  
+                        
+                        target_id = message.mentions[0].id
+                        amount = message.content.split()[2]
+                        c.execute('UPDATE users SET balance = balance + ? WHERE user_id = ?', (amount, str(target_id)))
+                        conn.commit()
+
+                        await message.channel.send(f'Gave <@{target_id}> ${amount}')
+                    else:
+                        await message.channel.send('You don\'t have permission to do that!')
+
+
 
